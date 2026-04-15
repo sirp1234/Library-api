e3ad82ba472147044c36335094f30f47cad37c6a
Paul okafor <{email}>
2026-04-15 21:03:40 +0100

    fix port issue for Render

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ app.use('/api/users', authRoutes);
 
 // Start the server
 const PORT =process.env.PORT || 3000;
-app.listen(3000, () => {
-    console.log(`Server is running on port 3000`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
